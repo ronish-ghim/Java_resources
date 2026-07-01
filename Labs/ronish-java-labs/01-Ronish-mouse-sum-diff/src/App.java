b8158c684a476177ae334f5488041e12f4b60a74
Ronish Ghimire <iamrg2060@gmail.com>
2026-07-01 17:38:34 +0545

    Rename: Ronish-xx-lab-title to xx-Ronish-lab-title

--- Labs/ronish-java-labs/01-Ronish-mouse-sum-diff/src/App.java
+++ Labs/ronish-java-labs/01-Ronish-mouse-sum-diff/src/App.java
@@ -3,18 +3,25 @@ import java.awt.event.*;
 
 public class App extends Frame implements MouseListener {
     TextField t1, t2, result;
-    Label l;
+    Label l1, l2, l;
 
     public App() {
         setLayout(new FlowLayout());
+        l1 = new Label("Number 1");
+        l2 = new Label("Number 2");
         t1 = new TextField(10);
         t2 = new TextField(10);
-        result = new TextField(15);
+        result = new TextField(10);
         l = new Label("Result");
-        add(t1); add(t2); add(l); add(result);
+        add(l1);
+        add(t1);
+        add(l2);
+        add(t2);
+        add(l);
+        add(result);
         addMouseListener(this);
         setSize(300, 200);
-        setTitle("Mouse Sum Diff");
+        setTitle("Ronish 32");
         setVisible(true);
     }
 
@@ -30,9 +37,14 @@ public class App extends Frame implements MouseListener {
         result.setText(String.valueOf(a - b));
     }
 
-    public void mouseClicked(MouseEvent e) {}
-    public void mouseEntered(MouseEvent e) {}
-    public void mouseExited(MouseEvent e) {}
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
 
     public static void main(String[] args) {
         new App();
